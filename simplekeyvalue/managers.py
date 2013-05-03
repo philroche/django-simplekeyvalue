8042d5663523922342f76ba44c0c5317b80cd161
phil.roche <phil.roche@ticket-text.com>
2013-05-03 16:08:37 +0100

    added optional co-owner to KeyValue

--- simplekeyvalue/managers.py
+++ simplekeyvalue/managers.py
@@ -6,52 +6,82 @@ from django.conf import settings
 class KeyValueManager(models.Manager):
 
 
-    def has_keyvalue(self, owner, key):
+    def has_keyvalue(self, owner=None, key=None, co_owner=None):
         """
         Test if a given owner has an instance of
         keyvalue for all the names provided
         """
-        qs = self.get_query_set()
-        owner_type = ContentType.objects.get_for_model(owner)
-        return qs.filter(owner_content_type=owner_type,
-                                       owner_object_id=owner.id,
-                                       key=key).exists()
+        if owner and key:
+            qs = self.get_query_set()
+            filter_args = {'owner_content_type': ContentType.objects.get_for_model(owner),
+                           'owner_object_id': owner.id,
+                           'co_owner_content_type': None,
+                           'co_owner_object_id': None,
+                           'key': key }
 
-    def get_keyvalue(self, owner, key=None, select_for_update=True):
+            if co_owner:
+                filter_args['co_owner_content_type'] = ContentType.objects.get_for_model(co_owner)
+                filter_args['co_owner_object_id'] = co_owner.id
+
+            return qs.filter(**filter_args).exists()
+        else:
+            return False
+
+    def get_keyvalue(self, owner=None, key=None, co_owner=None, select_for_update=True):
         """
         Get keyvalue instances for a given owner
         and filter with an optional names
         """
+        if owner and key:
+            filter_args = {'owner_content_type': ContentType.objects.get_for_model(owner),
+                           'owner_object_id': owner.id,
+                           'co_owner_content_type': None,
+                           'co_owner_object_id': None,
+                           'key': key }
+            if co_owner:
+                filter_args['co_owner_content_type'] = ContentType.objects.get_for_model(co_owner)
+                filter_args['co_owner_object_id'] = co_owner.id
 
-        filter_args = {'owner_content_type': ContentType.objects.get_for_model(owner),
-                       'owner_object_id': owner.id,
-                       'key': key}
+            qs=self.get_query_set().get(**filter_args)
+            if select_for_update:
+                qs = qs.select_for_update()
 
-        qs=self.get_query_set().get(**filter_args)
-        if select_for_update:
-            qs = qs.select_for_update()
-
-        return qs
+            return qs
+        else:
+            return self.get_query_set().none()
 
 
-    def set_keyvalue(self, owner, key, value):
+    def set_keyvalue(self, owner=None, key=None, value=None, co_owner=None):
 
+        if owner and key:
+            kwargs = {
+                'key': key,
+                'owner_content_type': ContentType.objects.get_for_model(owner),
+                'owner_object_id': owner.id,
+                'co_owner_content_type': None,
+                'co_owner_object_id': None
+            }
+            if co_owner:
+                kwargs['co_owner_content_type'] = ContentType.objects.get_for_model(co_owner)
+                kwargs['co_owner_object_id'] = co_owner.id
+            kv = self.get_or_create(**kwargs)[0]
+            kv.value = value
+            kv.save()
+            return kv
+        else:
+            return self.get_query_set().none()
 
-        kwargs = {
-            'key': key,
-            'owner_content_type': ContentType.objects.get_for_model(owner),
-            'owner_object_id': owner.id,
-            'value': value
-        }
-
-        return self.get_or_create(**kwargs)[0]
-
-    def del_keyvalue(self, owner, key):
+    def del_keyvalue(self, owner=None, key=None, co_owner=None):
         """
         Delete a list of keyvalue instances for a given user.
         """
-        owner_type = ContentType.objects.get_for_model(owner)
-        qs = self.get_query_set()
-        qs.filter(owner_content_type=owner_type,
-                  owner_object_id=owner.id,
-                  key=key).delete()
+        filter_args = {'owner_content_type': ContentType.objects.get_for_model(owner),
+                           'owner_object_id': owner.id,
+                           'co_owner_content_type': None,
+                           'co_owner_object_id': None,
+                           'key': key }
+        if co_owner:
+            filter_args['co_owner_content_type'] = ContentType.objects.get_for_model(co_owner)
+            filter_args['co_owner_object_id'] = co_owner.id
+
+        return self.get_query_set().filter(**filter_args).delete()
--- simplekeyvalue/models.py
+++ simplekeyvalue/models.py
@@ -10,7 +10,7 @@ class KeyValue(models.Model):
 
     """
     class Meta:
-        unique_together = (("owner_content_type","owner_object_id", "key"),)
+        unique_together = (("owner_content_type","owner_object_id","co_owner_content_type","co_owner_object_id", "key"),)
         #only works in django 1.5+
         #index_together = [
         #    ["owner_content_type","owner_object_id", "key"],
@@ -27,16 +27,26 @@ class KeyValue(models.Model):
     owner_content_object = generic.GenericForeignKey('owner_content_type',
                                                      'owner_object_id')
 
+    co_owner_content_type = models.ForeignKey(ContentType,
+                                           related_name="keyvalue_coowners", db_index=True, null=True, blank=True)
+
+    co_owner_object_id = models.PositiveIntegerField(db_index=True, null=True, blank=True)
+
+    co_owner_content_object = generic.GenericForeignKey('co_owner_content_type',
+                                                     'co_owner_object_id')
+
     key = models.CharField(max_length=50,help_text="Key the KeyValue", db_index=True)
 
-    value = models.IntegerField()
+    value = models.IntegerField(null=True, blank=True)
 
     @property
     def owner(self):
         return self.owner_content_object
 
+    @property
+    def cp_owner(self):
+        return self.co_owner_content_object
+
     def __unicode__(self):
-        if not self.description:
-            return self.name
-        return u'%s - %s' % (self.name, self.description)
+        return u'%d - %s' % (self.id, self.key)
 
